f0f2cd03da8a4b26ecbbd381ccc0da4307cc66ea
kanutan93 <d.morozov@dev.vtb.ru>
2024-03-06 14:01:01 +0200

    add frequent_crime_types

--- src/main/scala/BostonCrimesApp.scala
+++ src/main/scala/BostonCrimesApp.scala
@@ -1,31 +1,75 @@
+import org.apache.spark.sql
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.{btrim, col, split}
+import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.functions.{btrim, col, collect_list, concat_ws, count, countDistinct, desc, grouping, lit, percentile_approx, row_number, split}
 
 
 object BostonCrimesApp {
   def main(args: Array[String]): Unit = {
-    val spark = SparkSession.builder()
+    val sparkSession = SparkSession.builder()
       .appName("BostonCrimesApp")
       .config("spark.master", "local")
       .getOrCreate()
 
-    val offenseCodes = spark.read
+    val crimeDataFrame = getCrimeDataFrame(sparkSession)
+    val offenseCodeDataFrame = getOffenseCodeDataFrame(sparkSession)
+    val frequentCrimeTypesDataFrame = getFrequentCrimeTypesDataFrame(crimeDataFrame, offenseCodeDataFrame)
+
+    val result = crimeDataFrame
+      .groupBy("DISTRICT")
+      .agg(
+        countDistinct(col("*")).as("crimes_total"),
+//        percentile_approx((col("MONTH")), lit(0.5), lit(1000)).as("crimes_monthly"),
+
+      )
+      .join(frequentCrimeTypesDataFrame, frequentCrimeTypesDataFrame.col("DISTRICT") === crimeDataFrame.col("DISTRICT"), "inner")
+      .drop(frequentCrimeTypesDataFrame.col("DISTRICT"))
+      .select("DISTRICT", "crimes_total", "frequent_crime_types")
+    result.show()
+
+    sparkSession.stop()
+  }
+
+  private def getCrimeDataFrame(sparkSession: SparkSession): sql.DataFrame = {
+    sparkSession.read
       .option("header", true)
-      .schema("CODE STRING, NAME STRING")
+      .option("inferSchema", true)
+      .csv("bostoncrimes/crime.csv")
+  }
+
+  private def getOffenseCodeDataFrame(sparkSession: SparkSession): sql.DataFrame = {
+    sparkSession.read
+      .option("header", true)
+      .option("inferSchema", true)
       .csv("bostoncrimes/offense_codes.csv")
+  }
+
+  private def getFrequentCrimeTypesDataFrame(crimeDataFrame: sql.DataFrame, offenseCodeDataFrame: sql.DataFrame): sql.DataFrame = {
+    val crimeOffenseCodeDataFrame = crimeDataFrame
+      .join(offenseCodeDataFrame, crimeDataFrame.col("OFFENSE_CODE") === offenseCodeDataFrame.col("CODE"), "inner")
       .withColumn("split_NAME", split(col("NAME"), "-"))
       .select(
-        col("CODE"),
-        col("NAME"),
-        btrim(col("split_NAME").getItem(0)).as("SHORT_NAME")
+        col("DISTRICT"),
+        btrim(col("split_NAME").getItem(0)).as("crime_type")
       )
 
-    offenseCodes.show()
+    val crimeOffenseCodeDataFramePartitioned = crimeOffenseCodeDataFrame
+      .groupBy("DISTRICT", "crime_type")
+      .agg(
+        count("*").as("crime_count")
+      )
+      .withColumn(
+        "row",
+        row_number().over(Window.partitionBy("DISTRICT").orderBy(col("crime_count").desc))
+      )
 
-    spark.stop()
-    //
-    //  offenseCodes
-    //    .take(10)
-    //    .foreach(println)
+    crimeOffenseCodeDataFramePartitioned
+      .filter(col("row") <= 3)
+      .groupBy("DISTRICT")
+      .agg(collect_list("crime_type").as("frequent_crime_types_list"))
+      .select(
+        col("DISTRICT"),
+        concat_ws(",", col("frequent_crime_types_list")).as("frequent_crime_types")
+      )
   }
 }
